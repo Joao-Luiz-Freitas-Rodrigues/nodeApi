b958b9af5bbdd8b502f766ea565dca735a7537e6
mac <mac@MacBook-Air-de-mac.local>
2019-04-25 14:32:20 -0300

    atualizacao de segurança

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const basicAuth = require('express-basic-auth');
+
+
 
 
 //iniciando a aplicação
